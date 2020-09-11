559efe757053e219067bfded57089f69d0616ac9
Yoan Hristov <ioan.xristov@abv.bg>
2020-09-11 20:01:41 +0300

    working next question and question number almost :D

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,8 +1,15 @@
 <template>
   <div>
     <b-nav>
-      <b-nav-item disabled>Don`t remember here</b-nav-item>
-      <b-nav-item disabled>Current 4/10</b-nav-item>
+      <b-nav-item disabled>Fancy Quiz App</b-nav-item>
+      <b-nav-item disabled>Current {{ index + 1 }}/10</b-nav-item>
     </b-nav>
   </div>
 </template>>
+<script>
+export default {
+  props: {
+    index: Number
+  }
+}
+</script>
--- src/components/QuestionBox.vue
+++ src/components/QuestionBox.vue
@@ -1,16 +1,27 @@
 <template>
   <div>
     <b-jumbotron>  
-      <template v-slot:lead>
-        Some question here?
+      <template v-slot:lead v-b>
+        {{ currentQuestion.question }}
       </template>
       <hr class="my-4">
       <p>
         List of answers
       </p>
   
-      <b-button variant="primary" href="#">Submit</b-button>
-      <b-button variant="success" href="#">Next question</b-button>
+      <b-button variant="primary">Submit</b-button>
+      <b-button @click="next" variant="success">Next question</b-button>
     </b-jumbotron>
   </div>
 </template>>
+<script>
+export default {
+  props: {
+    currentQuestion: Object,
+    next: Function
+  },
+  mounted() {
+    console.log(this.currentQuestion)
+  }
+}
+</script>
